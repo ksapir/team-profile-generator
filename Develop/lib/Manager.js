497310f7d97565f30bc1a4eef8a5b0634a6f2522
Karen Pion <karenlpion@gmail.com>
2021-07-20 03:23:29 -0400

    employee class partial

--- Develop/lib/Manager.js
+++ Develop/lib/Manager.js
@@ -1,1 +1,20 @@
 // TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
+const Employee = require ('./Employee')
+
+class Manager extends Employee{
+    constructor(name,officeNumber,email){
+    super(name,135,email);
+    this.officeNumber = officeNumber;
+    }
+    getRole(){
+        return "Manager"
+    }
+    getOffice(){
+        return this.officeNumber;
+    }
+}
+
+const larry = new Manager("Karen", 170, "k@k.k");
+larry.printEmployee();
+larry.getOffice()
+module.exports = Manager
